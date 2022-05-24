d2950c7516239d4723b11a143700030212795d96
fckppag <87424710+fckppag@users.noreply.github.com>
2022-05-24 18:52:42 +0300

    Update progects.go

--- package/models/progects.go
+++ package/models/progects.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type ProjectCore struct {
-	ID     string
+	ID     uint
 	Name   string
 	Author string
 	Date   time.Time
@@ -15,4 +18,23 @@ type ProjectPageCore struct {
 	Description  string
 	Preview      string
 	LinkScratch  string
+}
+
+type ProjectDB struct {
+	gorm.Model
+
+	Name   string `gorm:"not null;size:256"`
+	Author string `gorm:"not null;size:256"`
+	Date   string `gorm:"not null;size:256"`
+	Json   string `gorm:"not null;size:65535"`
+}
+
+type ProgectPageDB struct {
+	gorm.Model
+
+	PPId        uint
+	PP          ProjectDB `gorm:"foreignKey:PPId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Description string    `gorm:"size:256;not null"`
+	Preview     string    `gorm:"size:256;not null"`
+	LinkScratch string    `gorm:"size:256;not null"`
 }
