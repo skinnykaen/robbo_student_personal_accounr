00d69990c7edf301352a459b170ee4783371c9f9
fckppag <87424710+fckppag@users.noreply.github.com>
2022-05-25 14:26:01 +0300

    mapDB

--- package/models/user.go
+++ package/models/user.go
@@ -32,3 +32,41 @@ func (em *UserDB) FromCore(user *UserCore) {
 	em.Email = user.Email
 	em.Password = user.Password
 }
+
+func (em *ProjectDB) ToCore() *ProjectCore {
+	return &ProjectCore{
+		ID:     strconv.FormatUint(uint64(em.ID), 10),
+		Name:   em.Name,
+		Author: em.Author,
+		Date:   em.Date,
+		Json:   em.Json,
+	}
+}
+
+func (em *ProjectDB) FromCore(project *ProjectCore) {
+	id, _ := strconv.ParseUint(project.ID, 10, 64)
+	em.ID = uint(id)
+	em.Name = project.Name
+	em.Author = project.Author
+	em.Date = project.Date
+	em.Json = project.Json
+}
+
+func (em *ProjectPageDB) ToCore(projects []*ProjectDB) ProjectPageCore {
+	var coreProjects []*ProjectCore
+	for _, projectDB := range projects {
+		coreProjects = append(coreProjects, projectDB.ToCore())
+	}
+	return ProjectPageCore{
+		Description: em.Description,
+		Preview:     em.Preview,
+		LinkScratch: em.LinkScratch,
+		ProjectsCore:    coreProjects,
+	}
+}
+
+func (em *ProjectPageDB) FromCore(pp *ProjectPageCore) {
+	em.Description = pp.Description
+	em.Preview = pp.Preview
+	em.LinkScratch = pp.LinkScratch
+}
