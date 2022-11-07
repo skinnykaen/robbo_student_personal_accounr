a09616e11a34e5531fe1f641171383b57b4a2b19
sasha <sasha.nazaroff2016@gmail.com>
2022-11-07 13:26:58 +0300

    added checks for user, robbogroup, robbounit

--- package/resolvers/robboGroup.go
+++ package/resolvers/robboGroup.go
@@ -5,30 +5,79 @@ package resolvers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/skinnykaen/robbo_student_personal_account.git/package/models"
 )
 
 // GetRobboGroupByID is the resolver for the GetRobboGroupById field.
 func (r *queryResolver) GetRobboGroupByID(ctx context.Context, id string) (*models.RobboGroupHTTP, error) {
+	ginContext, err := GinContextFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(err)
+	userId, userRole, identityErr := r.authDelegate.UserIdentity(ginContext)
+	if identityErr != nil {
+		return nil, identityErr
+	}
+	fmt.Println(userId)
+	fmt.Println(userRole)
+	fmt.Println(identityErr)
 	robboGroupsHttp, err := r.robboGroupDelegate.GetRobboGroupById(id)
 	return &robboGroupsHttp, err
 }
 
 // GetRobboGroupsByTeacherID is the resolver for the GetRobboGroupsByTeacherId field.
 func (r *queryResolver) GetRobboGroupsByTeacherID(ctx context.Context, teacherID string) ([]*models.RobboGroupHTTP, error) {
+	ginContext, err := GinContextFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(err)
+	userId, userRole, identityErr := r.authDelegate.UserIdentity(ginContext)
+	if identityErr != nil {
+		return nil, identityErr
+	}
+	fmt.Println(userId)
+	fmt.Println(userRole)
+	fmt.Println(identityErr)
 	robboGroupsHttp, err := r.robboGroupDelegate.GetRobboGroupsByTeacherId(teacherID)
 	return robboGroupsHttp, err
 }
 
 // GetRobboGroupsByRobboUnitID is the resolver for the GetRobboGroupsByRobboUnitId field.
 func (r *queryResolver) GetRobboGroupsByRobboUnitID(ctx context.Context, robboUnitID string) ([]*models.RobboGroupHTTP, error) {
+	ginContext, err := GinContextFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(err)
+	userId, userRole, identityErr := r.authDelegate.UserIdentity(ginContext)
+	if identityErr != nil {
+		return nil, identityErr
+	}
+	fmt.Println(userId)
+	fmt.Println(userRole)
+	fmt.Println(identityErr)
 	robboGroupsHttp, err := r.robboGroupDelegate.GetRobboGroupsByRobboUnitId(robboUnitID)
 	return robboGroupsHttp, err
 }
 
 // SearchGroupsByName is the resolver for the SearchGroupsByName field.
 func (r *queryResolver) SearchGroupsByName(ctx context.Context, name string) ([]*models.RobboGroupHTTP, error) {
+	ginContext, err := GinContextFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(err)
+	userId, userRole, identityErr := r.authDelegate.UserIdentity(ginContext)
+	if identityErr != nil {
+		return nil, identityErr
+	}
+	fmt.Println(userId)
+	fmt.Println(userRole)
+	fmt.Println(identityErr)
 	robboGroupsHttp, err := r.robboGroupDelegate.SearchRobboGroupByName(name)
 	return robboGroupsHttp, err
 }
--- package/resolvers/robboUnit.go
+++ package/resolvers/robboUnit.go
@@ -5,24 +5,61 @@ package resolvers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/skinnykaen/robbo_student_personal_account.git/package/models"
 )
 
 // GetRobboUnitByID is the resolver for the GetRobboUnitById field.
 func (r *queryResolver) GetRobboUnitByID(ctx context.Context, id string) (*models.RobboUnitHTTP, error) {
+	ginContext, err := GinContextFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(err)
+	userId, userRole, identityErr := r.authDelegate.UserIdentity(ginContext)
+	if identityErr != nil {
+		return nil, identityErr
+	}
+	fmt.Println(userId)
+	fmt.Println(userRole)
+	fmt.Println(identityErr)
 	robboUnitsHttp, err := r.robboUnitsDelegate.GetRobboUnitById(id)
 	return &robboUnitsHttp, err
 }
 
 // GetAllRobboUnits is the resolver for the GetAllRobboUnits field.
 func (r *queryResolver) GetAllRobboUnits(ctx context.Context) ([]*models.RobboUnitHTTP, error) {
+	ginContext, err := GinContextFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(err)
+	userId, userRole, identityErr := r.authDelegate.UserIdentity(ginContext)
+	if identityErr != nil {
+		return nil, identityErr
+	}
+	fmt.Println(userId)
+	fmt.Println(userRole)
+	fmt.Println(identityErr)
 	robboUnitsHttp, err := r.robboUnitsDelegate.GetAllRobboUnit()
 	return robboUnitsHttp, err
 }
 
 // GetRobboUnitsByUnitAdminID is the resolver for the GetRobboUnitsByUnitAdminId field.
 func (r *queryResolver) GetRobboUnitsByUnitAdminID(ctx context.Context, unitAdminID string) ([]*models.RobboUnitHTTP, error) {
+	ginContext, err := GinContextFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(err)
+	userId, userRole, identityErr := r.authDelegate.UserIdentity(ginContext)
+	if identityErr != nil {
+		return nil, identityErr
+	}
+	fmt.Println(userId)
+	fmt.Println(userRole)
+	fmt.Println(identityErr)
 	robboUnitsHttp, err := r.robboUnitsDelegate.GetRobboUnitsByUnitAdminId(unitAdminID)
 	return robboUnitsHttp, err
 }
