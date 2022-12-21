e58c1b398737af738ce151272f63e42f983b9f55
sasha <sasha.nazaroff2016@gmail.com>
2022-12-21 14:19:03 +0300

    Added TokenAuthMiddleware

---
+++ server/middleware.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/gin-gonic/gin"
+	"github.com/skinnykaen/robbo_student_personal_account.git/package/models"
+	"github.com/spf13/viper"
+	"net/http"
+	"strings"
+)
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		header := c.GetHeader("Authorization")
+		if header == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "token not found",
+			})
+			c.Abort()
+			return
+		}
+		headerParts := strings.Split(header, " ")
+		if len(headerParts) != 2 {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "token not found",
+			})
+			c.Abort()
+			return
+		}
+		data, err := jwt.ParseWithClaims(headerParts[1], &models.UserClaims{},
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(viper.GetString("auth.access_signing_key")), nil
+			})
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+			c.Abort()
+			return
+		}
+
+		claims, ok := data.Claims.(*models.UserClaims)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "token claims are not of type *StandardClaims",
+			})
+			c.Abort()
+			return
+		}
+		c.Set("user_id", claims.Id)
+		c.Set("user_role", claims.Role)
+		c.Next()
+	}
+}
