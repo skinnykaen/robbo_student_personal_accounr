13046686dfbcb424c8e8f38c71ac83893ff8407e
sasha <sasha.nazaroff2016@gmail.com>
2023-01-23 14:16:08 +0300

    added the container lifetime parameter to the test config

--- package/docker_client/docker.go
+++ package/docker_client/docker.go
@@ -39,7 +39,7 @@ func NewTestDockerClient() (testDockerClient dockertest.Pool, cleanerContainer f
 		log.Fatalf("Could not start resource: %s", err)
 	}
 
-	resource.Expire(100)
+	resource.Expire(viper.GetUint("container_lifetime"))
 	cleanerContainer = func() {
 		// purge the container
 		err = pool.Purge(resource)
