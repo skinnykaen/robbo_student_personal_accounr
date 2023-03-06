b9843c02eeb8c0bb8f55923ba98f56b2e0b76b76
Evgeniy Sushchevich <rupychman@mail.ru>
2023-03-06 18:29:59 +0300

    feat: procted project

--- package/projects/delegate.go
+++ package/projects/delegate.go
@@ -5,6 +5,6 @@ import "github.com/skinnykaen/robbo_student_personal_account.git/package/models"
 type Delegate interface {
 	CreateProject(project *models.ProjectHTTP) (id string, err error)
 	DeleteProject()
-	GetProjectById(projectId string) (project models.ProjectHTTP, err error)
+	GetProjectById(projectId, userId string) (project models.ProjectHTTP, err error)
 	UpdateProject(project *models.ProjectHTTP) (err error)
 }
